bdedbc3a67d859754480addc5866e9a251c03986
gamerus12125 <miska3040@gmail.com>
2025-09-12 21:24:02 +0300

    added fix for nvidia wayland

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    unsafe {
+        // Not unsafe if you don't use edition 2024
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
     calendar_app_lib::run()
 }
